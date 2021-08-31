a733c058d6c431c260cc88c3d65156e296e3921e
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-08-31 12:31:26 +0300

    added styling & tel markup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
+import { Form } from './ContactForm.styled';
 
-const ContactForm = props => {
+const ContactForm = ({ name, number, onInputChange, onSubmitButtonClick }) => {
   return (
-    <form action="submit">
-      <label htmlFor="">
+    <Form action="submit">
+      <label>
         Name
         <input
           type="text"
@@ -13,13 +13,34 @@ const ContactForm = props => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
           required
+          onChange={onInputChange}
+          value={name}
         />
       </label>
-      <button type="submit">Add contact</button>
-    </form>
+      <label>
+        Phone number
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          onChange={onInputChange}
+          value={number}
+        />
+      </label>
+      <button type="submit" onClick={onSubmitButtonClick}>
+        Add contact
+      </button>
+    </Form>
   );
 };
 
 export default ContactForm;
 
-ContactForm.propTypes = {};
+ContactForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSubmitButtonClick: PropTypes.func.isRequired,
+};
