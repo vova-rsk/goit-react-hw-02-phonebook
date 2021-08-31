0c7ee2b9dcfb5386b5d55f56b4c1ddd89ee9b12a
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-08-31 12:33:13 +0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,48 @@ import React, { Component } from 'react';
 import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList/ContactList';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
+  };
+
+  onSubmitButtonClick = e => {
+    this.setState(({ contacts, name }) => {
+      const contactToAdd = {
+        id: uuidv4(),
+        name,
+      };
+
+      return {
+        contacts: [...contacts, contactToAdd],
+        name: '',
+      };
+    });
+  };
+
+  onInputChange = e => {
+    const name = e.target.name;
+    this.setState({ [name]: e.target.value.trim() });
   };
 
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm
+          name={this.state.name}
+          number={this.state.number}
+          onInputChange={this.onInputChange}
+          onSubmitButtonClick={this.onSubmitButtonClick}
+        />
 
         <h2>Contacts</h2>
         <Filter />
-        <ContactList />
+        <ContactList contacts={this.state.contacts} />
       </div>
     );
   }
