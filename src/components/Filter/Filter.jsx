e4445f1306b0b446178f4d985000ab9daccc8286
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-08-31 14:37:23 +0300

    maked it working

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Label } from './Filter.styled';
 
-const Filter = props => {
-  return <div></div>;
+const Filter = ({ filter, onInputChange }) => {
+  return (
+    <Label>
+      Find contacts by name
+      <input
+        type="text"
+        name="filter"
+        onChange={onInputChange}
+        value={filter}
+      />
+    </Label>
+  );
 };
 
 export default Filter;
 
-Filter.propTypes = {};
+Filter.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    }),
+  ),
+  onInputChange: PropTypes.func.isRequired,
+};
