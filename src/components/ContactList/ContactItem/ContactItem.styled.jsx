da497e90b2bf0e385b1963f2001c7054cbb23db8
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2021-09-01 15:13:23 +0300

    fix

--- src/components/ContactList/ContactItem/ContactItem.styled.jsx
+++ src/components/ContactList/ContactItem/ContactItem.styled.jsx
@@ -35,7 +35,7 @@ const ListItem = styled.li`
     border: 0.5px solid #b7b7b7;
     border-radius: 4px;
     font-size: 11px;
-    line-height: 1.167;
+    line-height: 1;
 
     &:hover {
       -webkit-box-shadow: 0px 1px 3px 2px rgba(34, 60, 80, 0.2);
